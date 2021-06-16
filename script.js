829cf1ca46e7807e706328b717e067162f191187
gammafer <60357762+gammafer@users.noreply.github.com>
2021-06-16 18:41:15 +0200

    You can draw, and clear the grid now

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let container = document.getElementById('container');
+let clearButton = document.getElementById ('clearButton');
 
-for(let i=0; i<255;i++){
-    let gridPart = document.createElement('div');
-    gridPart.classList=`gridPart`;
-    container.appendChild(gridPart);
+function makeGrid(squarePerSide){
+    for(let i=0; i<(squarePerSide*squarePerSide);i++){
+            let gridPart = document.createElement('div');
+            gridPart.classList=`gridPart`;
+            gridPart.style.height=`${90/squarePerSide}vh`;
+            gridPart.style.width=`${100/squarePerSide}vw`;
+            container.appendChild(gridPart);
+            gridPart.addEventListener('mouseover',function(e){
+            e.target.style.backgroundColor="black"});
+    }
 }
+
+makeGrid(16);
+function clearGrid(){
+    container.innerHTML='';
+}
+clearButton.addEventListener('click',clearGrid);
