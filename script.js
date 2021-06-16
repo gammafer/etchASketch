ac7bf00059b7f202b6260bd57b000b948e5edda1
gammafer <60357762+gammafer@users.noreply.github.com>
2021-06-16 20:25:45 +0200

    You can clear and remake the grid now

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let container = document.getElementById('container');
-let clearButton = document.getElementById ('clearButton');
+let container =     document.getElementById('container');
+let navBar=         document.getElementById('navbar');
+let clearButton =   document.getElementById ('clearButton');
+let isGridOnline=true;
 
 function makeGrid(squarePerSide){
     for(let i=0; i<(squarePerSide*squarePerSide);i++){
@@ -13,8 +15,40 @@ function makeGrid(squarePerSide){
     }
 }
 
-makeGrid(16);
 function clearGrid(){
     container.innerHTML='';
+    input.hidden=false;
+    isGridOnline=false;
+    clearButton.innerText='Create my grid!';
+     
+    //Adding a placeholder to our sketchpad for fun.
+    let placeholder=document.createElement('p');
+    placeholder.innerHTML='Grid is currently offline :(';
+    container.appendChild(placeholder);
+    
+
+    /*let squarePerSide=100;
+    makeGrid(squarePerSide);*/
 }
-clearButton.addEventListener('click',clearGrid);
+
+function prepareForNewGrid(){
+    container.innerHTML='';
+    makeGrid(input.value);
+    input.hidden=true;
+    clearButton.innerText='Clear this thing';
+    isGridOnline=true;
+}
+function whatToDo(){
+    if(isGridOnline) clearGrid();
+    else prepareForNewGrid();
+}
+
+makeGrid(16);
+
+let input=document.createElement('input');
+    input.placeholder="Give me a number";
+    input.style.alignContent="center";
+    input.hidden=true;
+ navBar.insertBefore(input,navBar.firstChild);
+
+clearButton.addEventListener('click',  whatToDo);
